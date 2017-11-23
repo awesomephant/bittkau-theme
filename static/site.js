c9dfd9d8867078df1c6dc47239e17f3819542529
Max Koehler <max@koehler-kn.de>
2017-11-23 19:51:40 +0000

    build out homepage

--- static/site.js
+++ static/site.js
@@ -1,5 +1,28 @@
-jQuery( document ).ready( function( $ ) {
+console.log("hi");
+var generateToc = function() {
+  var headlines = document.querySelectorAll("#content .section-title");
+  var toc = "<li>Auf dieser Seite:</li>";
+  for (var i = 0; i < headlines.length; i++) {
+    let id = headlines[i].innerHTML
+      .replace(/ /g, "-")
+      .replace(/&nbsp;/g, "-")
+      .replace(/&amp;/g, "und")
+      .toLowerCase();
+    headlines[i].setAttribute("id", id);
+    toc +=
+      "<li><a class='article-toc--link' href='#" +
+      id +
+      "'>" +
+      headlines[i].innerHTML +
+      "</li>";
+  }
+  var el = document.createElement("ul");
+  el.classList.add("article-toc");
+  el.innerHTML = toc;
+  var post = document.querySelector(".site-nav");
+  if (headlines.length > 1) {
+    post.appendChild(el);
+  }
+};
 
-  // Your JavaScript goes here
-
-});
+generateToc();
